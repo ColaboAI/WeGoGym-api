1fb08ad7fb1ece47964b346826dca2169509ef81
GoGiants1 <chooi221@snu.ac.kr>
2022-08-25 12:46:29 +0900

    fix

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Add User Group"
-useradd -s /bin/bash -m ${USER_NAME} &&
-export HOME=/home/${USER_NAME} &&
-usermod -u ${USER_ID} ${USER_NAME} &&
-groupadd -g ${GROUP_ID} ${GROUP_NAME} && 
-usermod -g ${GROUP_NAME} ${USER_NAME} &&
+# useradd -s /bin/bash -m ${USER_NAME} &&
+# export HOME=/home/${USER_NAME} &&
+# usermod -u ${USER_ID} ${USER_NAME} &&
+# groupadd -g ${GROUP_ID} ${GROUP_NAME} && 
+# usermod -g ${GROUP_NAME} ${USER_NAME} &&
 
 
 echo "Dockerizing..."
