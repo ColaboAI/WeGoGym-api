27c866656b735dc9c1417541cb80b491b08e9374
GoGiants1 <chooi221@snu.ac.kr>
2023-01-10 19:24:43 +0900

    Add async redis connect function

--- app/core/helpers/redis.py
+++ app/core/helpers/redis.py
@@ -2,4 +2,26 @@ import redis.asyncio as aioredis
 
 from app.core.config import settings
 
-redis = aioredis.from_url(url=f"redis://{settings.REDIS_HOST}")
+redis = aioredis.from_url(
+    url=f"redis://{settings.REDIS_USERNAME}:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0",
+)
+
+
+async def async_redis_conn(self) -> str:
+    """
+    Assemble database URL from self.
+    :return: database URL.
+    """
+    return await aioredis.from_url(
+        "redis://"
+        + settings.REDIS_USERNAME
+        + ":"
+        + settings.REDIS_PASSWORD
+        + "@"
+        + settings.REDIS_HOST
+        + ":"
+        + settings.REDIS_PORT
+        + "/"
+        "0",
+        decode_responses=True,
+    )
