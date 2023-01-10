ce570a5a3682a8da0bf680f37edd60af0b94cb6a
GoGiants1 <chooi221@snu.ac.kr>
2023-01-10 16:28:09 +0900

    fix: remove invalid s3 syntax

--- app/utils/aws.py
+++ app/utils/aws.py
@@ -2,7 +2,6 @@ import boto3
 from app.core.config import settings
 
 s3_client = boto3.client(
-client = boto3.client(
     "s3",
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
