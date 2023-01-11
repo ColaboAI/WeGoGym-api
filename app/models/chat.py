763529bc6e46593a9869892edce5751d5b171039
GoGiants1 <chooi221@snu.ac.kr>
2023-01-11 18:00:13 +0900

    Remove unused type

--- app/models/chat.py
+++ app/models/chat.py
@@ -1,4 +1,4 @@
-from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String
+from sqlalchemy import TIMESTAMP, Boolean, Column, String
 from sqlalchemy.orm import relationship
 from app.models.base import Base
 import uuid
