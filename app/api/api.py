a1edc968d32b96adb4e009d2f872716de34a32da
sgeunhi <alsrb582@snu.ac.kr>
2023-01-12 19:26:18 +0900

    Remove fastapi-users

--- app/api/api.py
+++ app/api/api.py
@@ -1,43 +1,9 @@
 from fastapi import APIRouter
-from app.core.security import (
-    auth_backend,
-    google_oauth_client,
-)
-from app.api.deps import fastapi_users
-from app.schemas.user import UserRead, UserCreate, UserUpdate
+
 from app.api.routers.audio import audio_router
 
 
 api_router = APIRouter()
-
-api_router.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
-)
-api_router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-api_router.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
-api_router.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["auth"],
-)
-api_router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
-api_router.include_router(
-    fastapi_users.get_oauth_router(google_oauth_client, auth_backend, "SECRET"),
-    prefix="/auth/google",
-    tags=["auth"],
-)
 
 api_router.include_router(
     audio_router,
--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -6,5 +6,5 @@ echo "Run Alembic Migrations"
 echo "Create initial data in DB and Run FastAPI with gunicorn"
 alembic --raiseerr upgrade head &&\
 
-python -m app.initial_data && \
+# python -m app.initial_data && \
 gunicorn -b 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.main:app
