fa63f581edb9de4ec0157478c70793065678ffbc
GoGiants1 <chooi221@snu.ac.kr>
2023-01-13 14:55:03 +0900

    Fix redis conn func

--- app/core/helpers/redis.py
+++ app/core/helpers/redis.py
@@ -7,21 +7,13 @@ redis = aioredis.from_url(
 )
 
 
-async def async_redis_conn(self) -> str:
+async def get_redis_conn(self) -> str:
     """
     Assemble database URL from self.
     :return: database URL.
     """
     return await aioredis.from_url(
-        "redis://"
-        + settings.REDIS_USERNAME
-        + ":"
-        + settings.REDIS_PASSWORD
-        + "@"
-        + settings.REDIS_HOST
-        + ":"
-        + settings.REDIS_PORT
-        + "/"
-        "0",
+        url=f"redis://{settings.REDIS_USERNAME}:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0",
         decode_responses=True,
+        encoding="utf-8",
     )
