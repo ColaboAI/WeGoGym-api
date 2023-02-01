7fcc62bc1e42d892d29e36aeebbd307b694068e7
GoGiants1 <chooi221@snu.ac.kr>
2023-02-01 14:16:37 +0900

    Apply new session in ws

--- app/api/websockets/chat.py
+++ app/api/websockets/chat.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends, UploadFile, Form, HTTPException, WebSock
 from app.models.chat import ChatRoom
 from app.schemas.chat import ChatRoomRead
 
-from app.session import get_async_session
-from app.service.chat_service import ChatService, find_chat_room_by_id
+from app.session import get_db_transactional_session
+from app.services.chat_service import ChatService, find_chat_room_by_id
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.conn import conn_manager
 from sqlalchemy import select
@@ -18,12 +18,13 @@ async def chat_websocket_endpoint(
     websocket: WebSocket,
     chat_room_id: str,
     user_id: str,
-    session: AsyncSession = Depends(get_async_session),
+    session: AsyncSession = Depends(get_db_transactional_session),
 ):
     chat_room = await find_chat_room_by_id(chat_room_id, session)
     if not chat_room:
         raise HTTPException(status_code=404, detail="Chat room not found")
 
+    # chat_room_member = chat_room.members.filter_by(user_id=user_id).first()
     # check user is in chat room
 
     await conn_manager.connect(websocket)
