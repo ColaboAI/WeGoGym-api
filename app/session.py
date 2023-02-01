e4d2c18ad6c74a621ed8c77a95260e021485b379
GoGiants1 <chooi221@snu.ac.kr>
2023-02-01 14:12:07 +0900

    Add new async session maker and deps

--- app/session.py
+++ app/session.py
@@ -1,22 +1,70 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_scoped_session,
+)
 from typing import AsyncGenerator
 from sqlalchemy.orm.session import sessionmaker
 from app.core import config
+from sqlalchemy import exc
 
 sqlalchemy_database_uri = ""
 if config.settings.ENVIRONMENT == "DEV":
     sqlalchemy_database_uri = config.settings.LOCAL_SQLALCHEMY_DATABASE_URI
 else:
     sqlalchemy_database_uri = config.settings.DEFAULT_SQLALCHEMY_DATABASE_URI
-
 async_engine = create_async_engine(
     sqlalchemy_database_uri, pool_pre_ping=True, echo=True
 )
-async_session_maker = sessionmaker(
-    async_engine, expire_on_commit=False, class_=AsyncSession
+
+autocommit_engine = async_engine.execution_options(isolation_level="AUTOCOMMIT")
+autocommit_session_factory = async_scoped_session(
+    sessionmaker(
+        autocommit_engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    ),
+    scopefunc=current_task,
+)
+transactional_session_factory = async_scoped_session(
+    sessionmaker(
+        async_engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    ),
+    scopefunc=current_task,
 )
 
-# database adapter dep
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
+
+async def get_db_transactional_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Create and get database session.
+    :param request: current request.
+    :yield: database session.
+    """
+    session: AsyncSession = transactional_session_factory()
+
+    try:  # noqa: WPS501
         yield session
+    except exc.DBAPIError:
+        await session.rollback()
+    finally:
+        await session.commit()
+        await session.close()
+
+
+async def get_db_autocommit_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Create and get database session.
+    :param request: current request.
+    :yield: database session.
+    """
+    session: AsyncSession = autocommit_session_factory()
+
+    try:  # noqa: WPS501
+        yield session
+    except exc.DBAPIError:
+        await session.rollback()
+    finally:
+        await session.close()
