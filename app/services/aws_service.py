3447e5f5148b5fe352f5575641b2a633ba1af27f
GoGiants1 <chooi221@snu.ac.kr>
2023-02-16 16:08:53 +0900

    feat: Add file content type in boto3 upload logic

--- app/services/aws_service.py
+++ app/services/aws_service.py
@@ -9,11 +9,14 @@ def upload_image_to_s3(file: UploadFile, uid: UUID) -> str:
 
     img_url = None
     try:
-        image_key = f"profile_pic/{uid}/{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        image_key = f"profile_pic/{uid}/{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}"
         s3_client.upload_fileobj(
             file.file,
             bucket_name,
             image_key,
+            ExtraArgs={
+                "ContentType": file.content_type,
+            },
         )
         img_url = f"https://{bucket_name}.s3.amazonaws.com/{image_key}"
     except Exception as e:
