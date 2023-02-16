9f29ff36416d2abce5b0d1c7b026894cfdc211d8
GoGiants1 <chooi221@snu.ac.kr>
2023-02-16 19:39:11 +0900

    feat: Add initial workout promise codes

---
+++ app/schemas/workout_promise.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class ParticipantStatus(str, Enum):
+    PENDING = "PENDING"
+    ACCEPTED = "ACCEPTED"
+    REJECTED = "REJECTED"
+    CANCELLED = "CANCELLED"
+
+
+class WorkoutParticipantBase(BaseModel):
+    status: ParticipantStatus
+    created_at: datetime
+    updated_at: datetime
+
+
+class WorkoutParticipantCreate(WorkoutParticipantBase):
+    pass
+
+
+class WorkoutParticipantUpdate(WorkoutParticipantBase):
+    pass
+
+
+class WorkoutParticipantRead(WorkoutParticipantBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class WorkoutPromiseBase(BaseModel):
+    title: str
+    description: str
+    start_time: datetime
+    end_time: datetime
+    created_at: datetime
+    updated_at: datetime
+
+
+class WorkoutPromiseCreate(WorkoutPromiseBase):
+    pass
+
+
+class WorkoutPromiseUpdate(WorkoutPromiseBase):
+    pass
+
+
+class WorkoutPromiseRead(WorkoutPromiseBase):
+    id: int
+    max_participants: int
+    participants: list[WorkoutParticipantRead]
+
+    class Config:
+        orm_mode = True
