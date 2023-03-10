e1113e37bb5181c5bf313dab0e897f89998c0703
GoGiants1 <chooi221@snu.ac.kr>
2023-03-10 14:25:41 +0900

    feat: Add token validation for websocket endpoint - Delete broadcasting function

--- app/core/conn.py
+++ app/core/conn.py
@@ -1,9 +1,35 @@
+from uuid import UUID
 from fastapi import WebSocket
+import jwt
+from app.core.exceptions.token import DecodeTokenException, ExpiredTokenException
 
+from app.utils.token_helper import TokenHelper
 
+# Add connection manager method to validate user auth
 class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[str, WebSocket] = {}
+
+    async def validate_user(self, user_id: UUID, token: str):
+        # token is jwt token
+        if not token:
+            return False
+        try:
+            payload = TokenHelper.decode(
+                token,
+            )
+            user_id_from_token: UUID | None = payload.get("user_id")
+        except jwt.exceptions.PyJWTError:
+            return False
+        except ExpiredTokenException:
+            return False
+        except DecodeTokenException:
+            return False
+        if not user_id:
+            return False
+        if user_id != user_id_from_token:
+            return False
+        return True
 
     async def connect(self, key: str, websocket: WebSocket):
         await websocket.accept()
@@ -11,13 +37,6 @@ class ConnectionManager:
 
     def disconnect(self, key: str):
         del self.active_connections[key]
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections.values():
-            await connection.send_json(message, mode="text")
 
 
 conn_manager = ConnectionManager()
