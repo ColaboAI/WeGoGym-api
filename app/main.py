a9e7bec2fb6d9e14ad288efe42e1e4c69bbec452
GoGiants1 <chooi221@snu.ac.kr>
2023-03-14 00:52:57 +0900

    feat: Add event handler

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import asyncio
+import signal
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
@@ -5,6 +7,7 @@ from fastapi.middleware import Middleware
 from fastapi.responses import JSONResponse
 
 from app.api.api import api_router
+from app.core import conn
 from app.core.config import settings
 from app.core.exceptions.base import CustomException
 from app.core.helpers.cache import Cache, RedisBackend, CustomKeyMaker
@@ -24,6 +27,9 @@ def init_cache() -> None:
     Cache.init(backend=RedisBackend(), key_maker=CustomKeyMaker())  # type: ignore
 
 
+HANDLED_SIGNALS = (signal.SIGINT, signal.SIGTERM)
+
+
 def init_listeners(app_: FastAPI) -> None:
     # Exception handler
     @app_.exception_handler(CustomException)
@@ -32,6 +38,31 @@ def init_listeners(app_: FastAPI) -> None:
             status_code=exc.code,
             content={"error_code": exc.error_code, "message": exc.message},
         )
+
+    # TODO: Remove this(only for debug)
+    @app_.exception_handler(RequestValidationError)
+    async def validation_exception_handler(
+        request: Request, exc: RequestValidationError
+    ):
+        exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+        print(f"{request}: {exc_str}")
+        content = {"status_code": 10422, "message": exc_str, "detail": exc.errors()}
+
+        print(content)
+        return JSONResponse(
+            content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+        )
+
+    # Graceful shutdown
+    @app_.on_event("shutdown")
+    async def shutdown_event():
+        print("Shutting down...")
+        await conn.conn_manager.close_all()
+        print("All connections closed.")
+        print("Stop event loop...")
+        loop = asyncio.get_running_loop()
+        loop.stop()
+        print("Event loop stopped.")
 
 
 def on_auth_error(request: Request, exc: Exception):
@@ -81,15 +112,3 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
-
-# TODO: Remove this(only for debug)
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
-    print(f"{request}: {exc_str}")
-    content = {"status_code": 10422, "message": exc_str, "detail": exc.errors()}
-
-    print(content)
-    return JSONResponse(
-        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
-    )
