907d1e9f25241a32eb3bd4306f5367d115a70d0a
sgeunhi <alsrb582@snu.ac.kr>
2023-03-23 15:28:33 +0900

    feat: add req user id

--- app/api/routers/notification.py
+++ app/api/routers/notification.py
@@ -29,11 +29,12 @@ notification_router = APIRouter()
     dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
 )
 async def get_notifications(
+    req: Request,
     session: AsyncSession = Depends(get_db_transactional_session),
     limit: int = Query(10, description="Limit"),
     offset: int = Query(0, description="offset"),
 ):
-    total, n_list = await get_notification_list(session, limit, offset)
+    total, n_list = await get_notification_list(session, req.user.id, limit, offset)
 
     return {
         "total": total,
@@ -44,28 +45,28 @@ async def get_notifications(
     }
 
 
-# 알림 정보 생성 엔드포인트
-@notification_router.post(
-    "",
-    response_model=NotificationRead,
-    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
-)
-async def create_notification(
-    notification: NotificationBase,
-    session: AsyncSession = Depends(get_db_transactional_session),
-):
-    return await create_notification(session, notification)
+# # 알림 정보 생성 엔드포인트
+# @notification_router.post(
+#     "",
+#     response_model=NotificationRead,
+#     dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+# )
+# async def create_notification(
+#     notification: NotificationBase,
+#     session: AsyncSession = Depends(get_db_transactional_session),
+# ):
+#     return await create_notification(session, notification)
 
 
-# 알림 정보 수정 엔드포인트
-@notification_router.put(
-    "/{notification_id}",
-    response_model=NotificationRead,
-    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
-)
-async def update_notification_by_id(
-    notification_id: UUID,
-    notification: NotificationUpdate,
-    session: AsyncSession = Depends(get_db_transactional_session),
-):
-    return await update_notification_by_id(session, notification_id, notification)
+# # 알림 정보 수정 엔드포인트
+# @notification_router.put(
+#     "/{notification_id}",
+#     response_model=NotificationRead,
+#     dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+# )
+# async def update_notification_by_id(
+#     notification_id: UUID,
+#     notification: NotificationUpdate,
+#     session: AsyncSession = Depends(get_db_transactional_session),
+# ):
+#     return await update_notification_by_id(session, notification_id, notification)
