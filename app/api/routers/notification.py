1a5ed0f34f97220ae09042a1ca8a0413c91c9a00
sgeunhi <alsrb582@snu.ac.kr>
2023-03-25 01:07:22 +0900

    feat: fix noti-router

--- app/api/routers/notification.py
+++ app/api/routers/notification.py
@@ -8,33 +8,34 @@ from app.core.fastapi.dependencies.premission import (
 )
 
 from app.schemas.notification import (
-    NotificationBase,
-    NotificationRead,
-    NotificationUpdate,
-    NotificationListResponse,
+    NotificationWorkoutBase,
+    NotificationWorkoutRead,
+    NotificationWorkoutListResponse,
 )
 
 from app.services.notification_service import (
-    get_notification_list,
+    get_notification_workout_list,
 )
 from app.session import get_db_transactional_session
 
 notification_router = APIRouter()
 
 
-# 알림 정보 조회 엔드포인트
+# 운동 알림 정보 조회 엔드포인트
 @notification_router.get(
-    "",
-    response_model=NotificationListResponse,
+    "/workout",
+    response_model=NotificationWorkoutListResponse,
     dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
 )
-async def get_notifications(
+async def get_notifications_workout(
     req: Request,
     session: AsyncSession = Depends(get_db_transactional_session),
     limit: int = Query(10, description="Limit"),
     offset: int = Query(0, description="offset"),
 ):
-    total, n_list = await get_notification_list(session, req.user.id, limit, offset)
+    total, n_list = await get_notification_workout_list(
+        session, req.user.id, limit, offset
+    )
 
     return {
         "total": total,
@@ -43,30 +44,3 @@ async def get_notifications(
         if total and total > offset + len(n_list)
         else None,
     }
-
-
-# # 알림 정보 생성 엔드포인트
-# @notification_router.post(
-#     "",
-#     response_model=NotificationRead,
-#     dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
-# )
-# async def create_notification(
-#     notification: NotificationBase,
-#     session: AsyncSession = Depends(get_db_transactional_session),
-# ):
-#     return await create_notification(session, notification)
-
-
-# # 알림 정보 수정 엔드포인트
-# @notification_router.put(
-#     "/{notification_id}",
-#     response_model=NotificationRead,
-#     dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
-# )
-# async def update_notification_by_id(
-#     notification_id: UUID,
-#     notification: NotificationUpdate,
-#     session: AsyncSession = Depends(get_db_transactional_session),
-# ):
-#     return await update_notification_by_id(session, notification_id, notification)
