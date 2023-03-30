d68f8d85c8054a1de1dc1316bf913b5136d827c4
GoGiants1 <chooi221@snu.ac.kr>
2023-03-30 15:44:45 +0900

    build: Change docker settings to uvicorn

--- docker/api/startup.prod.sh
+++ docker/api/startup.prod.sh
@@ -5,4 +5,4 @@ echo "Create initial data in DB and Run FastAPI with gunicorn"
 alembic --raiseerr upgrade head &&\
 
 # python -m app.initial_data && \
-gunicorn -b 0.0.0.0:80 -w 4 -k uvicorn.workers.UvicornWorker app.main:app
+uvicorn app.main:app --host 0.0.0.0 --port 8000
