d1bb29cfc497b7b942c45ab33eddabc1736367ad
GoGiants1 <chooi221@snu.ac.kr>
2023-04-10 22:18:51 +0900

    feat: add reason field in voc route

--- app/schemas/voc.py
+++ app/schemas/voc.py
@@ -9,10 +9,11 @@ class VOCBase(BaseModel):
     plaintiff: str | None = Field(None, title="신고자", description="신고자")
     defendant: str | None = Field(None, title="피신고자", description="피신고자")
     content: str = Field(..., title="내용", description="내용")
+    reason: str | None = Field(None, title="신고 사유", description="신고 사유")
 
 
 class VOCRequest(VOCBase):
     def __str__(self) -> str:
         return f"""
-        신고 유형: {self.type}\n신고자: {self.plaintiff}\n신고대상: {self.defendant}\n내용: {self.content}
+        신고 유형: {self.type}\n신고자: {self.plaintiff}\n신고대상: {self.defendant}\n신고 사유: {self.reason}\n내용: {self.content}
         """
