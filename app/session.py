c3dceec6ad3bca10701d80ff3530b847a2b18909
GoGiants1 <chooi221@snu.ac.kr>
2023-08-28 03:30:05 +0900

    fix: sqlalchemy session

--- app/session.py
+++ app/session.py
@@ -1,33 +1,16 @@
-from asyncio import current_task
 from functools import wraps
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    create_async_engine,
-    async_scoped_session,
-)
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from typing import AsyncGenerator
-from sqlalchemy.orm.session import sessionmaker
 from app.core import config
 from sqlalchemy import exc
 from app.utils.ecs_log import logger
 
 sqlalchemy_database_uri = config.settings.DEFAULT_SQLALCHEMY_DATABASE_URI
-async_engine = create_async_engine(
-    sqlalchemy_database_uri, pool_pre_ping=True, echo=True
-)
+async_engine = create_async_engine(sqlalchemy_database_uri, pool_pre_ping=True, echo=True)
 
-autocommit_engine = async_engine.execution_options(isolation_level="AUTOCOMMIT")
-autocommit_session_factory = async_scoped_session(
-    sessionmaker(
-        autocommit_engine,
-        expire_on_commit=False,
-        class_=AsyncSession,
-        future=True,
-    ),
-    scopefunc=current_task,
-)
-transactional_session_factory = sessionmaker(
-    async_engine, expire_on_commit=False, class_=AsyncSession, future=True
+transactional_session_factory = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False,
 )
 
 
@@ -48,40 +31,21 @@ async def get_db_transactional_session() -> AsyncGenerator[AsyncSession, None]:
         await session.close()
 
 
-async def get_db_autocommit_session() -> AsyncGenerator[AsyncSession, None]:
-    """
-    Create and get database session.
-    :param request: current request.
-    :yield: database session.
-    """
-    session: AsyncSession = autocommit_session_factory()
-
-    try:  # noqa: WPS501
-        yield session
-    except exc.DBAPIError:
-        await session.rollback()
-    finally:
-        await session.close()
-
-
 class Transactional:
     def __call__(self, func):
-        session: AsyncSession = transactional_session_factory()
-
         @wraps(func)
         async def _transactional(*args, **kwargs):
-            if kwargs.get("session", None):
-                return await func(*args, **kwargs)
-            try:
-                kwargs["session"] = session
-                result = await func(*args, **kwargs)
-                return result
-            except Exception as e:
-                logger.exception(f"{type(e).__name__} : {str(e)}")
-                await session.rollback()
-                raise e
-            finally:
-                await session.commit()
-                await session.close()
+            async with transactional_session_factory() as session:
+                if kwargs.get("session", None):
+                    return await func(*args, **kwargs)
+                try:
+                    kwargs["session"] = session
+                    result = await func(*args, **kwargs)
+                    await session.commit()
+                    return result
+                except Exception as e:
+                    logger.exception(f"{type(e).__name__} : {str(e)}")
+                    await session.rollback()
+                    raise e
 
         return _transactional
